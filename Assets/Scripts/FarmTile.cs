4e17a2084959cad393da4fddf2aa0132aebe2e3e
pokodzima <52672768+pokodzima@users.noreply.github.com>
2022-11-04 23:44:33 +0600

    Farm Tile and Plant prototype

---
+++ Assets/Scripts/FarmTile.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmTile : MonoBehaviour
+{
+    private Plant _seededPlant;
+    
+    public void SeedTile(Plant plantToSeed)
+    {
+        if (_seededPlant != null) return;
+        _seededPlant = plantToSeed;
+        _seededPlant.SpawnVisual(transform.position);
+    }
+
+#if UNITY_EDITOR
+    private void OnMouseDown()
+    {
+        
+    }
+#endif
+}
